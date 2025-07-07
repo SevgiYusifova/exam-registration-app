862462cb2ac4381452c2e58d9f98d79a60085836
Sevgi <sevgi.yusifova@smartsolutions.az>
2025-07-07 21:14:22 +0400

    initial app with navbar and modules

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './modules/students/students.component';
+import { SubjectsComponent } from './modules/subjects/subjects.component';
+import { ExamsComponent } from './modules/exams/exams.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'students', pathMatch: 'full' },
+  { path: 'students', component: StudentsComponent },
+  { path: 'subjects', component: SubjectsComponent },
+  { path: 'exams', component: ExamsComponent },
+  { path: '**', redirectTo: 'students' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './modules/students/students.component';
+import { SubjectsComponent } from './modules/subjects/subjects.component';
+import { ExamsComponent } from './modules/exams/exams.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    SubjectsComponent,
+    ExamsComponent
   ],
   imports: [
     BrowserModule,
